279c84c2fef13f3384667a34815fde860e3d5f96
Nicolas Urrego Guasca <nicolas.guasca16@gmail.com>
2024-01-21 15:32:34 -0700

    Added Account Tab Screen

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -1,41 +1,112 @@
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import { supabase } from "../../utils/supabase";
+import React, { useState } from "react";
+import { Text, View, Dimensions, TextInput } from "react-native";
+
+const { width, height } = Dimensions.get("window");
 
 export default function AccountScreen() {
+  const [email, setEmail] = useState<string>("");
   return (
     <View
       style={{
         flex: 1,
         backgroundColor: "white",
         alignItems: "center",
-        justifyContent: "center",
+        //justifyContent: "center",
       }}
     >
-      <Text style={styles.title}>ACCOUNT</Text>
-      <View style={styles.separator} />
-      <TouchableOpacity
-        style={{ marginTop: 100, backgroundColor: "red" }}
-        onPress={() => supabase.auth.signOut()}
+      <View
+        style={{
+          flexDirection: "row",
+          backgroundColor: "#F1F1F1",
+          borderRadius: 20,
+          height: 40,
+          width: width * 0.85,
+          alignItems: "center",
+          marginTop: 40,
+          overflow: "hidden",
+        }}
       >
-        <Text>SIGNNN OUTT</Text>
-      </TouchableOpacity>
+        <Text
+          style={{
+            color: "black",
+            width: width * 0.22,
+            paddingLeft: 20,
+          }}
+        >
+          Email
+        </Text>
+        <Text
+          style={{
+            paddingLeft: 20,
+            paddingRight: 20,
+            color: "gray",
+          }}
+        >
+          johnsmith24@gmail.com
+        </Text>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          backgroundColor: "#F1F1F1",
+          borderRadius: 20,
+          height: 40,
+          width: width * 0.85,
+          alignItems: "center",
+          marginTop: 10,
+          overflow: "hidden",
+        }}
+      >
+        <Text
+          style={{
+            color: "black",
+            width: width * 0.22,
+            paddingLeft: 20,
+          }}
+        >
+          Password
+        </Text>
+        <Text
+          style={{
+            paddingLeft: 20,
+            paddingRight: 20,
+            color: "gray",
+          }}
+        >
+          ********
+        </Text>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          backgroundColor: "#F1F1F1",
+          borderRadius: 20,
+          height: 40,
+          width: width * 0.85,
+          alignItems: "center",
+          marginTop: 10,
+          overflow: "hidden",
+        }}
+      >
+        <Text
+          style={{
+            color: "black",
+            width: width * 0.22,
+            paddingLeft: 20,
+          }}
+        >
+          Phone
+        </Text>
+        <Text
+          style={{
+            paddingLeft: 20,
+            paddingRight: 20,
+            color: "gray",
+          }}
+        >
+          +1 (123) 456-7890
+        </Text>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
--- components/headers/AccountHeader.tsx
+++ components/headers/AccountHeader.tsx
@@ -15,7 +15,7 @@ export default function AccountHeader() {
     <View
       style={{
         backgroundColor: "white",
-        height: height * 0.4,
+        height: height * 0.43,
         shadowColor: "#000",
         shadowOffset: {
           width: 0,
@@ -30,14 +30,14 @@ export default function AccountHeader() {
         style={{
           flex: 1,
           backgroundColor: "white",
-          paddingTop: 60,
+          paddingTop: 55,
           paddingLeft: 20,
           paddingRight: 20,
         }}
       >
         <View
           style={{
-            backgroundColor: "lightgreen",
+            backgroundColor: "white",
             flexDirection: "row",
             justifyContent: "space-between",
           }}
@@ -47,6 +47,54 @@ export default function AccountHeader() {
           <Pressable onPress={() => router.push({ pathname: "/settings" })}>
             <Octicons name="gear" size={24} color="#8c8c8c" />
           </Pressable>
+        </View>
+        <View style={{ flex: 1, alignItems: "center", paddingTop: 5 }}>
+          <Image
+            source={require("../../assets/images/ProfilePlaceholder.jpg")}
+            style={{
+              width: width * 0.35,
+              height: 131,
+              paddingTop: 0,
+              paddingBottom: 0,
+              backgroundColor: "pink",
+              borderRadius: 100,
+            }}
+            resizeMode="contain"
+          />
+          <Text
+            style={{
+              fontSize: 24,
+              fontWeight: "bold",
+              color: "#CE3535",
+              paddingTop: 10,
+            }}
+          >
+            John Smith
+          </Text>
+          <View style={{ flexDirection: "row" }}>
+            <Ionicons name="location-outline" size={16} color="black" />
+            <Text style={{ paddingLeft: 5, color: "black" }}>
+              123 Fake Street, Fake City
+            </Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              paddingTop: 12,
+              width: width * 0.6,
+              backgroundColor: "white",
+            }}
+          >
+            <View style={{ alignItems: "center" }}>
+              <Text style={{ fontSize: 30, fontWeight: "600" }}>3</Text>
+              <Text>Favourites</Text>
+            </View>
+            <View style={{ alignItems: "center" }}>
+              <Text style={{ fontSize: 30, fontWeight: "600" }}>3</Text>
+              <Text>Reservations</Text>
+            </View>
+          </View>
         </View>
       </View>
     </View>
